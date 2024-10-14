3c256428f4c4d4088b6321cd07662b9a2b785256
Duckkeip <doggygerman@gmail.com>
2024-10-14 23:30:30 +0700

    Add files via upload

---
+++ xedai/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Shop Xe Dap Milk</title>
+    <link rel="stylesheet" type="text/css" href="style_css.css">
+</head>
+<body>
+    <div id="header">
+        <a href="#">Địa chỉ cửa hàng</a>
+        <a href="#">Hotline: 0123 456 789</a>
+        <a href="#">Chăm sóc khách hàng</a>
+    </div>
+    <div id="menu">
+        <a href="#"><img src="https://png.pngtree.com/png-vector/20220705/ourmid/pngtree-bicycle-logo-icon-silhouette-png-image_5683291.png" alt="Xe đạp" onclick="location.href=location.href;"></a>
+        <a href="#">SẢN PHẨM</a>
+        <a href="#">KHUYẾN MÃI</a>
+        <a href="#">DỊCH VỤ</a>
+        <a href="#">GIỎ HÀNG</a>
+    </div>
+    <div>
+        <div id="content">
+            <h2>BỘ SƯU TẬP XE ĐẠP 2024 </h2>
+            <hr>
+            <?php 
+                    include("view/content.php")
+                ?>
+        </div>
+    </div>
+    <div id="footer">
+        <span>THÔNG TIN
+        <hr>
+        <ul>
+            <li>Trụ sở: 100 Hải Thượng Lãn Ông, P.10, Quận 5, TP.HCM</li>
+            <li>Email: info@xedap.vn</li>
+            <li>ĐKKD/MST: 0313891315</li>
+        </ul>
+        </span>
+        <span>CHÍNH SÁCH
+        <hr>
+        <ul>
+            <li>Chế độ bảo hành</li>
+            <li>Khuyến mãi</li>
+            <li>Hướng dẫn trả góp</li>
+        </ul>
+        </span>
+        <span>KẾT NỐI
+        <hr>
+        <ul>
+            <li>Youtube</li>
+            <li>Zalo</li>
+            <li>Facebook</li>
+        </ul>
+        </span>
+    </div>
+</body>
+</html>
---
+++ xedai/view/content.php
@@ -0,0 +1,23 @@
+<?php 
+    $link = new mysqli("localhost", "root", "", "db_xedapmilk");
+    $sql = "SELECT * FROM sanpham";
+    $result = $link->query($sql);
+
+    while ($row = $result->fetch_assoc()) {
+?>
+
+<div style="display:table-row">
+    <a href="" target="blank" >
+        <img src="<?php echo $row['hinhanh']; ?>" style="width:200px;height:200px;float:right">
+    </a>
+    
+        <h3 class="product1-name"><?php echo $row['ten_sanpham']; ?></h3>
+        <p class="product1-code">Mã sản phẩm: <?php echo $row['sanpham_id']; ?></p>
+        <p class="product1-price">Giá: <?php echo number_format($row['gia'], 0, ',', '.'); ?> VND</p>   
+        <p class="product1-description">Mô tả: <?php echo $row['mo_ta']; ?></p>
+</div>   
+
+
+<?php 
+    }
+?>
